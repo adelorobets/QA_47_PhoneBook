009e056ad143c432fedb1e1120b8c824a0caf664
Adel Orobets <orobetsadel@gmail.com>
2025-06-06 21:09:38 +0300

    Refactored test structure and page objects; improved login/registration tests
    - Refactored BasePage to use AjaxElementLocatorFactory with safe element checks
    - Cleaned up HomePage, ContactsPage, LoginPage with consistent locators and actions
    - Added reusable methods for login/registration form handling
    - Improved test readability and removed duplication (LoginTests, RegistrationTest)
    - Fixed isContactsPageDisplayed() to use reliable locators
    - Added test for login with uppercase email
    - Investigated login bug: backend is case-sensitive during login but not during registration
    - Documented bug in test and created structured bug report with full stack trace

--- src/main/java/pages/AddPage.java
+++ src/main/java/pages/AddPage.java
@@ -1,13 +1,10 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 
 public class AddPage extends BasePage{
 
     public AddPage(WebDriver driver) {
         super(driver);
-        PageFactory.initElements(new AjaxElementLocatorFactory(driver, 10), this);
     }
 }
